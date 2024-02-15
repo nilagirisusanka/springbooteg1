3c243a909ebaa630915ac19ff7457cf7b03c8135
nilagirisusanka <159879583+nilagirisusanka@users.noreply.github.com>
2024-02-15 12:59:15 +0530

    Update Springbooteg1Application.java

--- src/main/java/org/example/springbooteg1/Springbooteg1Application.java
+++ src/main/java/org/example/springbooteg1/Springbooteg1Application.java
@@ -2,7 +2,7 @@ package org.example.springbooteg1;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+//@SpringBootApplication changes my main class as SpringBootApplication
 @SpringBootApplication
 public class Springbooteg1Application {
 
